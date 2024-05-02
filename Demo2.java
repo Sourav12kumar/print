b75dcf118ac0c7940a0eca437162f5c6b37ddccb
Sourav12kumar <staggereddot@gmail.com>
2024-05-02 16:11:35 +0530

    Add new line ....SK

--- Demo2.java
+++ Demo2.java
@@ -2,9 +2,9 @@ public class Demo2 {
     public static void main(String a[]){
 
         String name = "sourav";
-       // String name = new String(Sourav);
-       System.out.println(name + "kumar");
+       //String name = new String(Sourav);
+       System.out.println(name.length() + "kumar");
        System.out.println(name.charAt(5));
-       System.out.println(name.concat("kumbhakar"));
+       System.out.println(name.concat("kumar"));
     }
 }
