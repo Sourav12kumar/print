ade6496802bde556ccae462d00912b6e19a91ed6
Sourav Kumbhakar <108406599+Sourav12kumar@users.noreply.github.com>
2024-05-02 14:40:09 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,10 @@
+public class Demo2 {
+    public static void main(String a[]){
+
+        String name = "sourav";
+       // String name = new String(Sourav);
+       System.out.println(name + "kumar");
+       System.out.println(name.charAt(5));
+       System.out.println(name.concat("kumbhakar"));
+    }
+}
